c7b0c1a0bfab229882de503e057b7c931e223c51
quokka890 <louisvedula6@protonmail.com>
2025-07-23 16:17:12 +0100

    optimization

--- preinstall.sh
+++ preinstall.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 ## Variables ##
 DISK=/dev/sda
+part1="${DISK}1"
+part2="${DISK}2"
 
 ## Partitioning ##
 umount -A recursive /mnt
@@ -11,23 +13,23 @@ sgdisk --new=1:0+2.1G "${DISK}"
 sgdisk --typecode=1:ef00 "${DISK}"
 sgdisk --new=2:0:0 "${DISK}"
 partprobe "${DISK}"
-part1="${DISK}1"
-part2="${DISK}2"
 
 ## Formatting
 mkfs.fat -F32 "${part1}"
 mkfs.btrfs "${part2}"
 
 ## Creating subvolumes ##
-createsubvolumes() {
-    btrfs subvolume create /mnt/@
-    btrfs subvolume create /mnt/@home
-}
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
 
-mountsubvol() {
-    mount -o compress=zstd,subvol=@ "${part2}" /mnt
-    mkdir -p /mnt/home
-    mount -o compress=zstd,subvol=@home "${part2}" /mnt/home
-    mkdir -p /mnt/efi
-    mount "${part1}" /mnt/efi
-}
+mount -o compress=zstd,subvol=@ "${part2}" /mnt
+mkdir -p /mnt/home
+mount -o compress=zstd,subvol=@home "${part2}" /mnt/home
+mkdir -p /mnt/efi
+mount "${part1}" /mnt/efi
+
+## Package installation ##
+pacstrap -K /mnt base base-devel linux linux-firmware git btrfs-progs timeshift intel-ucode nvim networkmanager pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber openssh man sudo
+
+## Fstab ##
+genfstab -U /mnt >> /mnt/etc/fstab
