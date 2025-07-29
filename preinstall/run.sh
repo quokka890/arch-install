8f6813c10071fe310c340301576438c0582bedde
quokka890 <louisvedula6@protonmail.com>
2025-07-29 18:17:36 +0100

    fixed preinstall.sh

--- preinstall/run.sh
+++ preinstall/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-run() {
+preinstall() {
     local dir
     dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
     source "$dir/../functions/preinstall/preliminary_checks.sh"
